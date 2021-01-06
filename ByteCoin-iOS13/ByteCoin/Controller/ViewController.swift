96d5351bbce2b68567643658d8dda7ea8c79aba0
Jhoan Mauricio Vivas Rubiano <mauricio.vivas@gemalto.com>
2021-01-05 20:59:47 -0500

    Added Networking layer with Extension refactor

--- ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
+++ ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcointLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    var coinManager = CoinManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        coinManager.delegate = self
+        currencyLabel.text = coinManager.currencyArray[0]
+        coinManager.fecthCoinPrice(for:  coinManager.currencyArray[0])
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
+// MARK: - Picker delegates
+
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+}
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        coinManager.fecthCoinPrice(for:coinManager.currencyArray[row])
+    }
+    
+    
+}
+
+extension ViewController: CoinManagerDelegate {
+    
+    func didFetchCurrencyPrice(exchangeRate: ExchangeRate) {
+        DispatchQueue.main.async {
+            self.currencyLabel.text = exchangeRate.currentQuote
+            self.bitcointLabel.text = String(format: "%.2f", exchangeRate.rate)
+        }
+    }
+}
--- ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
+++ ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,73 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didFailWithError(error:Error)
+    func didFetchCurrencyPrice(exchangeRate: ExchangeRate)
+}
+
+extension CoinManagerDelegate{
+    func didFailWithError(error:Error){
+        
+    }
+    
+    func didFetchCurrencyPrice(exchangeRate: ExchangeRate){
+        
+    }
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "93834397-80E8-4490-A43A-DACA432DD3B6"
+    var delegate: CoinManagerDelegate?
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func fecthCoinPrice(for currency: String){
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            
+            let task  = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                }
+                
+                if let safeData = data {
+                    print(String(data:safeData,encoding: .utf8) ?? "NO DATA")
+                    if let exchangeData = self.parseExchangeRateJSON(with: safeData){
+                        self.delegate?.didFetchCurrencyPrice(exchangeRate: exchangeData)
+                    }
+                }
+                
+            }
+            
+            task.resume()
+        }
+        
+        
+    }
+    
+    func parseExchangeRateJSON(with  data: Data) -> ExchangeRate? {
+        let decoder = JSONDecoder()
+        do
+        {
+            return try decoder.decode(ExchangeRate.self, from: data)
+        }catch{
+            self.delegate?.didFailWithError(error: error)
+        }
+        return nil
+    }
+    
+}
+
+struct ExchangeRate: Codable {
+    let rate : Double
+    let currentQuote:String
+    enum CodingKeys: String, CodingKey {
+        case currentQuote = "asset_id_quote"
+        case rate
+    }
 }
